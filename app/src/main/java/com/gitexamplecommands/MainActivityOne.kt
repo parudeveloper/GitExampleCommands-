a14265b41446f6f920593d0f7722e25dae7f0fdb
parudeveloper <paramesh.developer@gmail.com>
2024-07-24 17:02:55 +0530

    Added MainActivityOne

--- app/src/main/java/com/gitexamplecommands/MainActivityOne.kt
+++ app/src/main/java/com/gitexamplecommands/MainActivityOne.kt
@@ -5,8 +5,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.gitexamplecommands.databinding.ActivityMainOneBinding
 
 class MainActivityOne : AppCompatActivity() {
+    lateinit var binding: ActivityMainOneBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
