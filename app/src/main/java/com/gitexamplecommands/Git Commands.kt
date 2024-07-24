b32a257a0744323ec924d0f7a3b33271a1537303
parudeveloper <100413898+navyareddy12946@users.noreply.github.com>
2024-07-25 00:18:52 +0530

    Commands Updated

--- app/src/main/java/com/gitexamplecommands/Git Commands.kt
+++ app/src/main/java/com/gitexamplecommands/Git Commands.kt
@@ -1,16 +1,15 @@
 package com.gitexamplecommands
 
-/*
-* git status
-*
-* git pull origin Mallangi_main
 
-*oino
-*cd
-*cd
-*cd
-*cd
-*cd
-*cd
-*cd
+/*
+Steps To Clone the Project from Git :
+
+Access Folder
+and run this Command
+git clone https://github.com/username/repository.git
+
+
+
+
+
 * */
