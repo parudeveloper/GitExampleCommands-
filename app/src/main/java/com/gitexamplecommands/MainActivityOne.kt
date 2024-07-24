42f53699ad8081067c85854492d318d5370758a0
parudeveloper <paramesh.developer@gmail.com>
2024-07-24 17:19:00 +0530

    Added Command File

--- app/src/main/java/com/gitexamplecommands/MainActivityOne.kt
+++ app/src/main/java/com/gitexamplecommands/MainActivityOne.kt
@@ -11,12 +11,7 @@ class MainActivityOne : AppCompatActivity() {
     lateinit var binding: ActivityMainOneBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main_one)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityMainOneBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
