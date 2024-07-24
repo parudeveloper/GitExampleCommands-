abea7105f53a9f66cf1514393f3cb3ddcf77ac3b
parudeveloper <100413898+navyareddy12946@users.noreply.github.com>
2024-07-25 00:27:30 +0530

    commands update

--- app/src/main/java/com/gitexamplecommands/Git Commands.kt
+++ app/src/main/java/com/gitexamplecommands/Git Commands.kt
@@ -8,6 +8,15 @@ Access Folder
 and run this Command
 git clone https://github.com/username/repository.git
 
+Steps to push the Code to Git
+
+git status // to check the status of changes
+git init // to check the current repository name
+git add . // to add all files to the current commit
+
+git pull origin main
+git push origin main
+
 
 
 
